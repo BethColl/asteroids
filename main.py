a6c56811025273ac1c5232295de401e61cc90a8c
Haal-ITae <BethLynn12100@gmail.com>
2025-01-26 16:33:43 -0600

    added player object

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 
 from constants import *
+from player import *
 
 def main():
 	pygame.init()
@@ -10,11 +11,13 @@ def main():
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	frame_limiter = pygame.time.Clock()
 	dt = 0
+	player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
 		screen.fill((0, 0, 0))
+		player.draw(screen)
 		pygame.display.flip()
 		frame_limiter.tick(60)
 		dt = (frame_limiter.get_time() / 100)
