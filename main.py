e713a4e25f5670b4795b118bedf32a233daf23ae
Haal-ITae <BethLynn12100@gmail.com>
2025-01-26 16:49:42 -0600

    cleaned up drawables and updatables with groups

--- main.py
+++ main.py
@@ -11,14 +11,19 @@ def main():
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	frame_limiter = pygame.time.Clock()
 	dt = 0
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+	Player.containers = (updatable, drawable)
 	player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
 		screen.fill((0, 0, 0))
-		player.draw(screen)
-		player.update(dt)
+		for object in drawable:
+			object.draw(screen)
+		for object in updatable:
+			object.update(dt)
 		pygame.display.flip()
 		frame_limiter.tick(60)
 		dt = (frame_limiter.get_time() / 100)
